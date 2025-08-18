34dfc94d09d3b3170cbd37fbe4e238002bffe5de
Levis155 <levismbui@gmail.com>
2025-08-18 12:07:33 +0300

    place all elements on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { Slider } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div className="app-wrapper">
+      <div className="drum-machine">
+        <div className="buttons-container">
+          <button>q</button>
+          <button>w</button>
+          <button>e</button>
+          <button>a</button>
+          <button>s</button>
+          <button>d</button>
+          <button>z</button>
+          <button>x</button>
+          <button>c</button>
+        </div>
+
+        <div className="controls-container">
+          <label className="power-switch">
+            <span className="slider">power</span>
+            <input type="checkbox" />
+          </label>
+
+          <div className="display"></div>
+
+          <div><Slider defaultValue={50} aria-label="Disabled slider" /></div>
+
+          <label className="bank-switch">
+            <span className="slider">bank</span>
+            <input type="checkbox" />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
