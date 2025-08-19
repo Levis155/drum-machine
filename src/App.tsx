b9296512b1bf192b57042a6c4d3032b42f1c06d8
Levis155 <levismbui@gmail.com>
2025-08-19 16:19:09 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <div className="drum-machine">
-        <div className="buttons-container">
+    <div className="vh-100 d-flex justify-content-center align-items-center bg-dark-gray">
+      <div className="drum-machine d-flex">
+        <div className="buttons-container w-50 h-100 d-grid">
           <button>q</button>
           <button>w</button>
           <button>e</button>
@@ -17,20 +17,28 @@ function App() {
           <button>c</button>
         </div>
 
-        <div className="controls-container">
-          <label className="power-switch">
-            <span className="slider">power</span>
-            <input type="checkbox" />
-          </label>
+        <div className="controls-container w-50 h-100 d-flex flex-column align-items-center justify-content-between justify-content-lg-center gap-lg-3">
+          <div className="switch-container">
+            <span>power</span>
+            <label className="power-switch">
+              <input type="checkbox" />
+              <span className="slider"></span>
+            </label>
+          </div>
 
-          <div className="display"></div>
+          <div className="display bg-medium-dark-gray d-flex justify-content-center"></div>
 
-          <div><Slider defaultValue={50} aria-label="Disabled slider" /></div>
+          <div className="slider-container">
+            <Slider defaultValue={50} size="small" />
+          </div>
 
-          <label className="bank-switch">
-            <span className="slider">bank</span>
-            <input type="checkbox" />
-          </label>
+          <div className="switch-container">
+            <span>bank</span>
+            <label className="bank-switch">
+              <input type="checkbox" />
+              <span className="slider"></span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
